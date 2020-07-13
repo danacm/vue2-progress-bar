41519068d9ea2889b9b00e0981d0effd5547845f
danacm <chen_dana@sina.cn>
2020-07-13 22:22:36 +0800

    updata example

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,71 @@
+<!--
+ * @Date: 2020-07-13 20:44:05
+ * @Author: dana_chen@sina.cn
+ * @LastEditors: dana_chen@sina.cn
+ * @LastEditTime: 2020-07-13 22:17:20
+ * @FilePath: \codeStudy\github\vue-progress-bar\vue-progress-bar\src\App.vue
+-->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <progress-bar :completed-steps="completedSteps" :total-steps="totalSteps" style="width:100%;">
-      <template v-slot:footer>
-        
-      </template>
-    </progress-bar>
-  </div>
+	<div id="app">
+		<h5>default</h5>
+		<progress-bar
+			:completed-steps="completedSteps"
+			:total-steps="totalSteps"
+			style="width:100%;"
+		>
+			<template v-slot:footer> </template>
+		</progress-bar>
+		<h5>width:400,height:40</h5>
+		<progress-bar
+			:completed-steps="completedSteps"
+			:total-steps="totalSteps"
+			:height="40"
+			:width="400"
+		>
+		</progress-bar>
+	</div>
 </template>
 
 <script>
-import ProgressBar from "./components/ProgressBar.vue";
+	import ProgressBar from './components/ProgressBar.vue'
 
-export default {
-  name: "App",
-  components: {
-    ProgressBar
-  },
-  data () {
-    return {
-      completedSteps:1,
-      totalSteps:10
-    }
-  }
-};
+	export default {
+		name: 'App',
+		components: {
+			ProgressBar,
+		},
+		data() {
+			return {
+				completedSteps: 1,
+				totalSteps: 10,
+				timer: null,
+			}
+		},
+		methods: {
+			startProgress() {
+				const self = this
+				if (this.timer) {
+					window.clearInterval(this.timer)
+				}
+				this.timer = setInterval(() => {
+					if (self.completedSteps === self.totalSteps) self.completedSteps = 0
+					self.completedSteps++
+				}, 1000)
+			},
+    },
+    mounted() {
+      this.startProgress()
+    },
+	}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,205 +1,236 @@
 <template>
-  <div class="progress-container" :style="containerStyle">
-    <div :style="innerBlockStyle">
-      <slot name="header"></slot>
-    </div>
-    <div class="svg-content">
-      <svg
-        class="progress-bar"
-        :viewBox="viewBox"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs>
-          <linearGradient
-            :id="'gradient' + _uid"
-            :x1="gradient.x1"
-            :y1="gradient.y1"
-            :x2="gradient.x2"
-            :y2="gradient.y2"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop offset="30%" :stop-color="startColor" />
-            <stop offset="100%" :stop-color="stopColor" />
-          </linearGradient>
-        </defs>
-        <line
-          x1="5"
-          y1="5"
-          :x2="width"
-          y2="5"
-          fill="transparent"
-          :stroke="innerStrokeColor"
-          :stroke-dasharray="circumference"
-          stroke-dashoffset="0"
-          :stroke-linecap="strokeLinecap"
-          :style="outerStyle"
-        />
-        <line
-          x1="5"
-          y1="5"
-          :x2="width"
-          y2="5"
-          :stroke="'url(#gradient' + _uid + ')'"
-          :stroke-dasharray="circumference"
-          stroke-dashoffset="0"
-          :stroke-linecap="strokeLinecap"
-          :style="innerStyle"
-        />
-      </svg>
-    </div>
-    <div :style="innerBlockStyle">
-      <slot name="footer"></slot>
-    </div>
-  </div>
+	<div class="progress-container" :style="containerStyle">
+		<div :style="innerBlockStyle" v-if="$slots.header">
+			<slot name="header"></slot>
+		</div>
+		<div class="svg-row">
+			<div class="svg-content">
+				<svg
+					class="progress-bar"
+					:viewBox="viewBox"
+					version="1.1"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<defs>
+						<linearGradient
+							:id="'gradient' + _uid"
+							:x1="gradient.x1"
+							:y1="gradient.y1"
+							:x2="gradient.x2"
+							:y2="gradient.y2"
+							gradientUnits="userSpaceOnUse"
+						>
+							<stop offset="30%" :stop-color="startColor" />
+							<stop offset="100%" :stop-color="stopColor" />
+						</linearGradient>
+					</defs>
+					<line
+						:x1="halfHight"
+						:y1="halfHight"
+						:x2="width"
+						:y2="halfHight"
+						fill="transparent"
+						:stroke="innerStrokeColor"
+						:stroke-dasharray="circumference"
+						stroke-dashoffset="0"
+						:stroke-linecap="strokeLinecap"
+						:style="outerStyle"
+					/>
+					<line
+						:x1="halfHight"
+						:y1="halfHight"
+						:x2="width"
+						:y2="halfHight"
+						:stroke="'url(#gradient' + _uid + ')'"
+						:stroke-dasharray="circumference"
+						stroke-dashoffset="0"
+						:stroke-linecap="strokeLinecap"
+						:style="innerStyle"
+					/>
+				</svg>
+			</div>
+			<div class="svg-tip">
+				<span v-if="$slots.tip"><slot name="tip"></slot></span>
+				<span v-else>{{ (completedSteps / totalSteps) * 100 }}%</span>
+			</div>
+		</div>
+		<div :style="innerBlockStyle" v-if="$slots.footer">
+			<slot name="footer"></slot>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "ProgressBar",
-  props: {
-    viewBox: {
-      type: true,
-      required: false,
-      default: "0 0 210 10"
-    },
-    totalSteps: {
-      type: Number,
-      required: true,
-      default: 10
-    },
-    completedSteps: {
-      type: Number,
-      required: true,
-      default: 0
-    },
-    startColor: {
-      type: String,
-      required: false,
-      default: "#73cbfe"
-    },
-    stopColor: {
-      type: String,
-      required: false,
-      default: "#458efd"
-    },
-    outerStrokeWidth: {
-      type: Number,
-      required: false,
-      default: 10
-    },
-    innerStrokeWidth: {
-      type: Number,
-      required: false,
-      default: 10
-    },
-    strokeLinecap: {
-      type: String,
-      required: false,
-      default: "round"
-    },
-    animateSpeed: {
-      type: Number,
-      required: false,
-      default: 1000
-    },
-    timingFunc: {
-      type: String,
-      required: false,
-      default: "linear"
-    },
-    innerStrokeColor: {
-      type: String,
-      required: false,
-      default: "#d6efff"
-    }
-  },
+	export default {
+		name: 'ProgressBar',
+		props: {
+			height: {
+				type: Number,
+				default: 10,
+			},
+			width: {
+				type: Number,
+				default: 200,
+			},
+			totalSteps: {
+				type: Number,
+				required: true,
+				default: 10,
+			},
+			completedSteps: {
+				type: Number,
+				required: true,
+				default: 0,
+			},
+			startColor: {
+				type: String,
+				required: false,
+				default: '#73cbfe',
+			},
+			stopColor: {
+				type: String,
+				required: false,
+				default: '#458efd',
+			},
+			// outerStrokeWidth: {
+			// 	type: Number,
+			// 	required: false,
+			// 	default: 10,
+			// },
+			// innerStrokeWidth: {
+			// 	type: Number,
+			// 	required: false,
+			// 	default: 10,
+			// },
+			strokeLinecap: {
+				type: String,
+				required: false,
+				default: 'round',
+			},
+			animateSpeed: {
+				type: Number,
+				required: false,
+				default: 1000,
+			},
+			timingFunc: {
+				type: String,
+				required: false,
+				default: 'linear',
+			},
+			innerStrokeColor: {
+				type: String,
+				required: false,
+				default: '#d6efff',
+			},
+		},
 
-  data() {
-    return {
-      gradient: {
-        x1: 0,
-        x2: this.width,
-        y1: 0,
-        y2: 0
-      },
-      gradientAnimation: null,
-      currentAngle: 0,
-      strokeDashoffset: this.width,
-      width: 200,
-      height: 10,
-      containerWidth: 200
-    };
-  },
+		data() {
+			return {
+				viewBox: '',
+				gradient: {
+					x1: 0,
+					x2: this.width,
+					y1: 0,
+					y2: 0,
+				},
+				gradientAnimation: null,
+				currentAngle: 0,
+				strokeDashoffset: this.width,
+				containerWidth: this.width,
+				outerStrokeWidth: this.height / 2,
+				innerStrokeWidth: this.height / 2,
+			}
+		},
 
-  computed: {
-    circumference() {
-      return this.width;
-    },
-    outerStyle() {
-      return {
-        height: `${this.height}px`,
-        width: `${this.width}px`,
-        strokeWidth: `${this.outerStrokeWidth}px`
-      };
-    },
-    innerStyle() {
-      return {
-        height: `${this.height}px`,
-        width: `${this.width}px`,
-        strokeWidth: `${this.innerStrokeWidth}px`,
-        strokeDashoffset: this.strokeDashoffset,
-        transition: `stroke-dashoffset ${this.animateSpeed}ms ${this.timingFunc}`
-      };
-    },
-    innerBlockStyle() {
-      return {
-        width: `${this.containerWidth}px`
-      };
-    },
-    containerStyle() {
-      return {
-        width: `${this.containerWidth}px`
-      };
-    },
-    innerWith() {
-      return (this.completedSteps / (this.totalSteps)) * this.width;
-    }
-  },
+		computed: {
+			circumference() {
+				return this.width
+			},
+			outerStyle() {
+				return {
+					height: `${this.height}px`,
+					width: `${this.width}px`,
+					strokeWidth: `${this.outerStrokeWidth}px`,
+				}
+			},
+			innerStyle() {
+				return {
+					height: `${this.height}px`,
+					width: `${this.width}px`,
+					strokeWidth: `${this.innerStrokeWidth}px`,
+					strokeDashoffset: this.strokeDashoffset,
+					transition: `stroke-dashoffset ${this.animateSpeed}ms ${this.timingFunc}`,
+				}
+			},
+			innerBlockStyle() {
+				return {
+					width: `${this.containerWidth}px`,
+				}
+			},
+			containerStyle() {
+				return {
+					width: `${this.containerWidth}px`,
+				}
+			},
+			innerWith() {
+				return (this.completedSteps / this.totalSteps) * this.width
+			},
+			halfHight() {
+				return this.height / 2
+			},
+		},
 
-  methods: {
-    gotoGradientPoint() {
-      this.gradient.x2 = this.innerWith;
-    },
-    changeProgress() {
-      this.strokeDashoffset = this.width - this.innerWith;
-    }
-  },
+		methods: {
+			gotoGradientPoint() {
+				this.gradient.x2 = this.innerWith
+			},
+			changeProgress() {
+				this.strokeDashoffset = this.width - this.innerWith
+			},
+		},
 
-  watch: {
-    totalSteps() {
-      this.changeProgress();
-    },
+		watch: {
+			totalSteps() {
+				this.changeProgress()
+			},
 
-    completedSteps() {
-      this.changeProgress();
-    }
-  },
+			completedSteps() {
+				this.changeProgress()
+			},
+		},
 
-  created() {
-    const self = this;
-    self.changeProgress();
-    self.$nextTick(() => {
-      self.containerWidth = document.getElementsByClassName(
-        "progress-container"
-      )[0].offsetWidth;
-    });
-  }
-};
+		created() {
+			const self = this
+			self.viewBox = `0 0 ${parseInt(self.width) + 10} ${self.height}`
+			self.changeProgress()
+			self.$nextTick(() => {
+				self.containerWidth = document.getElementsByClassName(
+					'progress-container'
+				)[0].offsetWidth
+			})
+		},
+	}
 </script>
 
-<style>
-.progress-container {
-  position: relative;
-}
+<style lang="scss">
+	.progress-container {
+		position: relative;
+	}
+	.svg-row {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		line-height: 100%;
+		.svg-content {
+			display: inline-block;
+			width: 80%;
+			margin-right: 2%;
+		}
+		.svg-tip {
+			display: inline-block;
+			width: 18%;
+			font-size: 1em;
+			text-align: left;
+		}
+	}
 </style>
